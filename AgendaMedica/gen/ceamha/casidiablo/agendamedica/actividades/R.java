f07fc3bff5c9bcb643f948c4c2c6d9d2cadf1a07
Cristian <Cristian@desarrollo-1-xp.(none)>
2010-03-16 22:22:41 -0500

    Se aniade el layout de actividades y se modifica el Manifiesto

--- AgendaMedica/gen/ceamha/casidiablo/agendamedica/actividades/R.java
+++ AgendaMedica/gen/ceamha/casidiablo/agendamedica/actividades/R.java
@@ -24,7 +24,7 @@ public final class R {
         public static final int salir=0x7f02000a;
     }
     public static final class id {
-        public static final int apellido_paciente=0x7f05000b;
+        public static final int apellido_paciente=0x7f05000c;
         public static final int apellidos_paciente=0x7f050001;
         public static final int cancelar_guardar_paciente=0x7f050008;
         public static final int correo_paciente=0x7f050005;
@@ -32,15 +32,17 @@ public final class R {
         public static final int documento_paciente=0x7f050002;
         public static final int fecha_nacimiento_paciente=0x7f050006;
         public static final int guardar_paciente=0x7f050007;
-        public static final int motivo_cita=0x7f050009;
-        public static final int nombre_paciente=0x7f05000a;
+        public static final int imagen=0x7f050009;
+        public static final int motivo_cita=0x7f05000a;
+        public static final int nombre_paciente=0x7f05000b;
         public static final int nombres_paciente=0x7f050000;
         public static final int telefono_paciente=0x7f050003;
     }
     public static final class layout {
         public static final int crear_paciente=0x7f030000;
-        public static final int lista_citas=0x7f030001;
-        public static final int lista_pacientes=0x7f030002;
+        public static final int despliegue_estadisticas=0x7f030001;
+        public static final int lista_citas=0x7f030002;
+        public static final int lista_pacientes=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
